d713a3c52a38964457fa00edce7f4a2b200b995d
IDalb <synfoniz@gmail.com>
2024-10-21 13:56:58 -0400

    Import barcode scan library and update manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
 }
